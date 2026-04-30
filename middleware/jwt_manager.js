52b7130dc07455bdf4c423793c5c3e7929dda57f
adiweb12 <vsadith051@gmail.com>
2026-04-30 14:01:40 +0530

    Update jwt_manager.js

--- middleware/jwt_manager.js
+++ middleware/jwt_manager.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-const { JWT_SECRET } = require("./configs/config");
+const { JWT_SECRET } = require("../configs/config");
 
 const authenticate = (req, res, next) => {
   const authHeader = req.headers.authorization;
